97bcd842a87dd80018b92c629248ee36dcf4051b
Sufyan03019777121 <156623479+Sufyan03019777121@users.noreply.github.com>
2025-04-28 00:04:26 +0500

    admin up36

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -6,7 +6,9 @@ function Details() {
   return (
     <div className="container mt-4">
       <h2>Product Details (ID: {id})</h2>
-      <p>Yahan par product ka detail ayega.</p>
+      <p>Yahan par product ka detail ayega.
+        
+      </p>
     </div>
   );
 }
