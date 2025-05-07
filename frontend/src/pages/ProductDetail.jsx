fe70ac5c656b9054e670ac6a82519452a1e147d3
Sufyan03019777121 <156623479+Sufyan03019777121@users.noreply.github.com>
2025-05-07 08:12:13 +0500

    Update ProductDetail.jsx ok

--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -1,5 +1,5 @@
-
-import { useParams, Link } from 'react-router-dom';
+import React from 'react';
+import {  Link } from 'react-router-dom';
 import { FaArrowLeft, FaPhone, FaWhatsapp } from 'react-icons/fa';
 
 function ProductDetail({ products }) {
